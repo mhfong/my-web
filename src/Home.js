fcad1f37f271bad1bca1b7c0789f738a93ac3751
mhfong <mhfong@connect.ust.hk>
2021-06-20 23:29:11 +0800

    update

---
+++ src/Home.js
@@ -0,0 +1,22 @@
+import React,{Component} from 'react';
+
+class Home extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div>
+        <section>
+          <h1>Section 1</h1>
+        </section>
+        <section>
+          <h1>Section 2</h1>
+        </section>
+        <section>
+          <h1>Section 3</h1>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Home;
